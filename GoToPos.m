1ed3693c553ed42bb34f662f4bd69e5f1429e48d
Fakhr-El-Din <fakhreldin.m.abdelkhalik@gmail.com>
2016-04-28 11:10:01 +0200

    Test motors on matlab and edit python for same order

--- GoToPos.m
+++ GoToPos.m
@@ -2,9 +2,9 @@ function [] = GoToPos( x,y,z,T,obj,gr )
     s = invKinematics(x,y,z,T,[0 0 0])
     fwrite(obj,strcat('0','0',int2str(1)));
     pause(0.1);
-    duty = goToDegree(s(1),s(2),s(3),s(4),s(5),0)
+    duty = goToDegree(s(1),s(2),s(3),s(4),s(5),'open')
     fwrite(obj,duty(1:9));
     pause(0.01)
-    fwrite(obj,strcat(duty(10:15),int2str(gr)));
+    fwrite(obj,strcat(duty(10:18)));
 end
 
