a992ecff7620f0cd4fd181847f827f65a56ff9c6
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2016-05-06 16:17:21 +0200

    Fix floating point error in python to give same results as matlab

--- Python/goToDegree.py
+++ Python/goToDegree.py
@@ -3,25 +3,25 @@ import numpy as np
 
 def goToDegree(deg1,deg2,deg3,deg4,deg5,deg6):
 
-    value1 = (133/45)*deg1 + 364 
+    value1 = (133.0/45.0)*deg1 + 364 
     if( value1 > 630 ):
         value1 = 630 
     elif( value1 < 110 ):
         value1 = 110 
 
-    value2 = (258/90)*deg2 + 130 ;
+    value2 = (258.0/90.0)*deg2 + 130 ;
     if( value2 > 650 ):
         value2 = 650 
     elif( value2 < 110 ):
         value2 = 110 
     
-    value3 = (127/45)*deg3 + 400 
+    value3 = (127.0/45.0)*deg3 + 400 
     if( value3 > 630 ):
         value3 = 630 
     elif( value3 < 110 ):
         value3 = 110 
     
-    value4 = (127/45)*deg4 + 386 
+    value4 = (127.0/45.0)*deg4 + 386 
     if( value4 > 630 ):
         value4 = 630 
     elif( value4 < 110 ):
@@ -51,3 +51,4 @@ def goToDegree(deg1,deg2,deg3,deg4,deg5,deg6):
     value6 = str(value6)
     
     return value1 + value2 + value6 + value4 + value5 + value3
+
