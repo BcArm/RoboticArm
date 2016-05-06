53b46ee3df33fc632ea5abbce77445184fa686b4
Fakhr-El-Din <fakhreldin.m.abdelkhalik@gmail.com>
2016-05-06 16:27:30 +0200

    Add 10 degrees to angle2 as offset to correct error

--- GoToPos.m
+++ GoToPos.m
@@ -2,7 +2,7 @@ function [] = GoToPos( x,y,z,T,obj,gr )
     s = invKinematics(x,y,z,T,[0 0 0])
     fwrite(obj,strcat('0','0',int2str(1)));
     pause(0.1);
-    duty = goToDegree(s(1),s(2),s(3),s(4),s(5),'open')
+    duty = goToDegree(s(1),s(2)+10,s(3),s(4),s(5),'open')
     fwrite(obj,duty(1:9));
     pause(0.01)
     fwrite(obj,strcat(duty(10:18)));
