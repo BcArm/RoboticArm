38bd0155fda376437cc936f55dbdb1f89d670e69
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2016-06-16 17:28:17 +0200

    Integrate callibration, kinect and gripper_center codes

--- Python/kinect_test.py
+++ Python/kinect_test.py
@@ -1,48 +1,62 @@
-#import the necessary modules
+import time
 import freenect
 import cv2
 import numpy as np
 import math
-from moveObj import moveObj
+from goToPosition import GoToPos
+from getGripperCenter import getGripperCenter
+from callibration import getTransformationMat
 
-from getGripperCenter import get getGripperCenter
+depth = []
+TRANS_MAT = np.matrix([])
+cv2.namedWindow('RGB image')
 
-def get_position(event, x, y, flags, param):
+def get_position(x, y):
+
+    sumx = 0
+    sumy = 0
+    sumz = 0      
+    
+    for i in range (0,20):
+        get_depth()
+
+        cv2.imshow('RGB image',frame)
+        cv2.imshow('Depth image', depth.astype(np.uint8))
+        
+        zw = 1 / ( ( (depth[y][x])*-0.0030711016 )+ 3.3309495161 )
+        zw = 100 * zw
+        sumz = sumz + zw
+        fx = 640 / (2 * math.tan(math.radians(57 / 2)))
+        xw = zw * (x - 319) / fx
+        sumx = sumx + xw
+        fy = 480 / (2 * math.tan(math.radians(43 / 2)))
+        yw = zw * (y - 239) / fy
+        sumy = sumy + yw
+
+    return [sumx/20, sumy/20, sumz/20]
+
+def go_to_position_mouse(event, x, y, flags, param):
+    
+    global TRANS_MAT
+
     if event == cv2.EVENT_LBUTTONDOWN:
-        sumx = 0
-        sumy = 0
-        sumz = 0      
-        for i in range (0,20):
-            get_depth()
-            cv2.imshow('RGB image',frame)
-            #display depth image
-            cv2.imshow('Depth image', depth.astype(np.uint8))
-            zw = 1 / ( ( (depth[y][x])*-0.0030711016 )+ 3.3309495161 )
-            zw = 100 * zw
-            sumz = sumz + zw
-            fx = 640 / (2 * math.tan(math.radians(57 / 2)))
-            xw = zw * (x - 319) / fx
-            sumx = sumx + xw
-            fy = 480 / (2 * math.tan(math.radians(43 / 2)))
-            yw = zw * (y - 239) / fy
-            sumy = sumy + yw
-        print sumx/20,sumy/20, sumz/20
+        
+        pos = get_position(x,y)    
+        
+        xk = pos[0]
+        yk = pos[1]
+        zk = pos[2]
+
+        print xk, yk, zk
+
+        point = TRANS_MAT * np.matrix([[xk],[yk],[zk],[1]])
+
+        GoToPos(point[0],point[1],point[2],'close')
+
         #print -(zw - 78), xw + 34, -(yw - 4) + 6.2
         #moveObj(-(zw - 78), xw + 34, -(yw - 4) + 6.2)
         #print -(zw - 81), xw + 30, -(yw - 0.5) + 6.7
         #GoToPos(-(zw - 81), xw + 30, -(yw - 0.5) + 6.7, 'open')
-        '''if flag == 1:
-            dif[0] = xw
-            dif[1] = yw
-            dif[2] = zw
-        elif flag == 0:
-            dif[0] -= xw
-            dif[1] -= yw
-            dif[2] -= zw
-            print dif[0],dif[1],dif[2]'''
-depth = []
-cv2.namedWindow('RGB image')
-cv2.setMouseCallback('RGB image', get_position)
 
 #function to get RGB image from kinect
 def get_video():
@@ -54,22 +68,44 @@ def get_video():
 def get_depth():
     global depth 
     depth, _ = freenect.sync_get_depth()
- 
-if __name__ == "__main__":
-    #get a frame from RGB camera
+
+#function to get coordinates of gripper center with respect to kinect frame
+def  get_kinect_frame_pt():
     frame = get_video()
+    gripper_centerXY = getGripperCenter(frame)
+    gripper_center = get_position(gripper_centerXY[0],gripper_centerXY[1])
+    return gripper_center
 
-    cimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    circles = cv2.HoughCircles(cimg,cv2.HOUGH_GRADIENT,1,20,param1=100,param2=100,minRadius=0,maxRadius=500)
-    circles = np.uint16(np.around(circles))
+if __name__ == "__main__":
     
-    for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    #callibrate kinect
+    kinect_frame_pts = []
 
-    cv2.imshow('detected circles',cimg)
+    GoToPos(-10,25,8.7,'close')
+    time.sleep(5)
+    kinect_frame_pts.append(get_kinect_frame_pt())
+    
+    GoToPos(0,21.22,12.86,'close')
+    time.sleep(5)
+    kinect_frame_pts.append(get_kinect_frame_pt())
+        
+    GoToPos(0,36,8.7,'close')
+    time.sleep(5)
+    kinect_frame_pts.append(get_kinect_frame_pt())
+    
+    GoToPos(10,21,15,'close')
+    time.sleep(5)
+    kinect_frame_pts.append(get_kinect_frame_pt())
+    
+    Kinect_frame_matrix = np.matrix([[kinect_frame_pts[0][0],kinect_frame_pts[1][0],kinect_frame_pts[2][0],kinect_frame_pts[3][0]],\
+                                    [kinect_frame_pts[0][1],kinect_frame_pts[1][1],kinect_frame_pts[2][1],kinect_frame_pts[3][1]],\
+                                    [kinect_frame_pts[0][2],kinect_frame_pts[1][2],kinect_frame_pts[2][2],kinect_frame_pts[3][2]],\
+                                    [1,1,1,1]])
+
+    TRANS_MAT = getTransformationMat(Kinect_frame_matrix)
+
+    cv2.setMouseCallback('RGB image', go_to_position_mouse)
+
     while 1:
         frame = get_video()
         #get a frame from depth sensor
